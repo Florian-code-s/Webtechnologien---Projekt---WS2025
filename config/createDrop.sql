564a6b16d325d4c2d865b6ad06627f307c57b4b4
JuliaMartinek <241445058+JuliaMartinek@users.noreply.github.com>
2025-12-23 16:18:54 +0100

    is_admin Attribut für user

--- config/createDrop.sql
+++ config/createDrop.sql
@@ -14,6 +14,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   `image_path` varchar(200) NOT NULL,
   `salt` varchar(20) NOT NULL,
   `password_hash` varchar(64) NOT NULL,
+  `is_admin` BOOLEAN NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`),
   UNIQUE KEY `unique_username` (`username`),
   UNIQUE KEY `unique_salt` (`salt`)
