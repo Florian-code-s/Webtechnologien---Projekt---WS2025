8b8e85af96e2db9058e6c4d1e0124c3712593e88
JuliaMartinek <241445058+JuliaMartinek@users.noreply.github.com>
2025-12-23 15:05:01 +0100

    Login mit Datenbank

--- pages/login.php
+++ pages/login.php
@@ -1,7 +1,28 @@
 <?php
 
-function checkCredentials($username, $password) {
-    if(strcmp($username, "User") == 0 && strcmp($password, "123") == 0) {
+require_once __DIR__ . '/../config/database.php';
+require_once __DIR__ . "/../functions/helpers.php";
+$error = "";
+
+function checkCredentials($conn, $username, $password)
+{
+    $sql = "SELECT salt, password_hash FROM `user` WHERE `username` = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("s", $username);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if ($result->num_rows !== 1) {
+        return false;
+    }
+
+    $row = $result->fetch_row();
+    $salt = $row[0];
+    $passwordHashFromDB = $row[1];
+
+    $hashedPassword = hashPassword($password, $salt);
+
+    if (strcmp($passwordHashFromDB, $hashedPassword) == 0) {
         return true;
     }
     return false;
@@ -10,48 +31,49 @@ function checkCredentials($username, $password) {
 if (!empty($_POST) && $_POST["username"] && $_POST["password"]) {
     $safeUsername = htmlspecialchars($_POST["username"], ENT_QUOTES, 'UTF-8');
     $safePassword = htmlspecialchars($_POST["password"], ENT_QUOTES, 'UTF-8');
-    if(checkCredentials($safeUsername, $safePassword)) {
+    $conn = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
+    if (checkCredentials($conn, $safeUsername, $safePassword)) {
         $_SESSION["user"] = $safeUsername;
         $_SESSION["logged_in"] = true;
         header("Location: ?page=home");
         exit;
+    } else {
+        $error = "Login nicht erfolgreich";
     }
 }
 ?>
 
-
-<!-- Denke nicht das das hier noetig ist but idk
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
-        crossorigin="anonymous"></script>-->
-    <section class="login">
-        <div class="d-flex justify-content-center align-items-center vh-100">
-            <div class="card text-center shadow login__card">
-                <div class="card-body">
-                    <h2 class="card-title mb-4 mt-2 login__card--title">Anmelden</h2>
-                    <div class="card-text">
-                        <form action="?page=login" method="POST"> 
-                            <div class="form-floating mb-3">
-                                <input type="text" class="form-control" id="username" name="username" placeholder="Benutzername"
-                                    required>
-                                <label for="username">Benutzername</label>
-                            </div>
-                            <div class="form-floating mb-3">
-                                <input type="password" class="form-control" id="password" name="password" placeholder="Passwort"
-                                    required>
-                                <label for="password">Passwort</label>
-                            </div>
-                            <button type="submit"
-                                class="btn btn-primary fw-semibold col-12 mb-3 login__button">Einloggen</button>
-                        </form>
-                        <p>
-                            Noch keinen Account? <a href="?page=register" class="login__link">Jetzt registrieren</a>
-                        </p>
-                    </div>
-
+<section class="login">
+    <div class="d-flex justify-content-center align-items-center vh-100">
+        <div class="card text-center shadow login__card">
+            <div class="card-body">
+                <h2 class="card-title mb-4 mt-2 login__card--title">Anmelden</h2>
+                <div class="card-text">
+                    <form action="?page=login" method="POST">
+                        <div class="form-floating mb-3">
+                            <input type="text" class="form-control" id="username" name="username"
+                                placeholder="Benutzername" required>
+                            <label for="username">Benutzername</label>
+                        </div>
+                        <div class="form-floating mb-3">
+                            <input type="password" class="form-control" id="password" name="password"
+                                placeholder="Passwort" required>
+                            <label for="password">Passwort</label>
+                        </div>
+                        <button type="submit"
+                            class="btn btn-primary fw-semibold col-12 mb-3 login__button">Einloggen</button>
+                    </form>
+                    <?php if (strcmp($error, "") !== 0): ?>
+                        <div class="alert alert-danger" role="alert">
+                            <?php echo $error; ?>
+                        </div>
+                    <?php endif; ?>
+                    <p>
+                        Noch keinen Account? <a href="?page=register" class="login__link">Jetzt registrieren</a>
+                    </p>
                 </div>
+
             </div>
         </div>
+    </div>
 </section>
-
-
